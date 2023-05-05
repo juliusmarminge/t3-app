502d10f68ba0bf3906b8b17b2cd476a5370d24ff
juliusmarminge <julius0216@outlook.com>
2023-05-05 14:12:32 +0200

    spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import { SubmitButton } from "./_components/form";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] pt-16">
+      <div className="container flex flex-col items-center gap-12 px-4 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
         </h1>
