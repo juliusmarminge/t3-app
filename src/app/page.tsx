e40835688a819b713526819e69fb8672b919ccb3
juliusmarminge <julius0216@outlook.com>
2023-05-05 14:28:08 +0200

    revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] pt-16">
       <div className="container flex flex-col items-center gap-12 px-4 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Create <span className="text-purple-400">T3</span> App
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
